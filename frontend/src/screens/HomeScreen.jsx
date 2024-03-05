112d81c5c445e5242660e60bc8310960083aca9e
Kartik <kartikagarwal20144@gmail.com>
2024-03-05 11:44:28 +0530

    Adding pagination for products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,10 +4,14 @@ import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Product from '../components/product'
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { useParams } from "react-router-dom";
+import Paginate from "../components/Paginate";
 
 
 const HomeScreen = () => {
-const {data:products,isLoading,error} = useGetProductsQuery();
+const {pageNumber} = useParams();
+
+const {data, isLoading, error} = useGetProductsQuery({pageNumber});
 
   return (
     <>
@@ -19,12 +23,16 @@ const {data:products,isLoading,error} = useGetProductsQuery();
     <>
     <h1>Latest Products </h1>
     <Row>
-      {products.map((product) => (
+      {data.products.map((product) => (
           <Col key={product._id} sm={12} md ={6} lg={4} xl={3}>
           <Product product={product}/>
           </Col>
           ))}
     </Row>
+    <Paginate 
+    pages={data.pages}
+    page={data.page}
+    />
     </>
     )}
     
