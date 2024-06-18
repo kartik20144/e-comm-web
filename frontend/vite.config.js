a645efb787d5d97f4eccf16812f7957cdc331a26
Kartik <kartikagarwal20144@gmail.com>
2024-06-18 23:24:42 +0530

    fixing bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-import path from "path"
-
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -23,4 +13,50 @@ export default defineConfig({
       },
     },
   },
-});
+})
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import dotenv from 'dotenv';
+// import path from "path"
+
+// dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import dotenv from 'dotenv';
+// import path from "path"
+
+// dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+//   build: {
+//     outDir: 'dist', // Ensure this is set correctly
+//     rollupOptions: {
+//       input: path.resolve(__dirname, 'index.html'),
+//     },
+//   },
+// });
